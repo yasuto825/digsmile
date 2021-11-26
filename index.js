c74c84bcb9961a98bc0a228a039e505bf0646dfb
yasuto825 <92569889+yasuto825@users.noreply.github.com>
2021-11-26 17:08:13 +0900

    変更

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ $(function () {
   $(".burger-wrapper").on("click", function () {
     $(".burger-btn").toggleClass("close");
     $(".nav-wrapper").animate({ width: "toggle" }, 300);
+    $(".body-mask").fadeToggle(600);
     $("body").toggleClass("noscroll");
   });
 });

ebbec40ee4de783429b31c44ec2dee4038ac875b
yasuto825 <92569889+yasuto825@users.noreply.github.com>
2021-11-27 14:45:32 +0900

    変更

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+// jQuery
 $(function () {
   //ハンバーガーメニューのアニメーション
   $(".burger-wrapper").on("click", function () {
     $(".burger-btn").toggleClass("close");
     $(".nav-wrapper").animate({ width: "toggle" }, 300);
     $(".body-mask").fadeToggle(600);
-    $("body").toggleClass("noscroll");
+  });
+
+  // ページ内リンクのアニメーション
+  $('a[href^="#"]').click(function () {
+    const speed = 500;
+    const href = $(this).attr("href");
+    const target = $(href == "#" || href == "" ? "html" : href);
+    const position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    return false;
   });
 });
